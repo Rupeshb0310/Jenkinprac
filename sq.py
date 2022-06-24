437ee5fe4898db7f6973574da38d29de959295a7
Rupeshb0310 <91204002+Rupeshb0310@users.noreply.github.com>
2022-06-24 19:34:36 +0530

    Add files via upload

---
+++ sq.py
@@ -0,0 +1,11 @@
+
+def reverse(num):
+    reversed_num = 0
+    while num != 0:
+        digit = num % 10
+        reversed_num = reversed_num * 10 + digit
+        num //= 10
+
+    print(reversed_num)
+
+reverse(9876)
